9027ed9845bc0be6c8ecfe3631d01c8b33ea2bd7
Kodochigov Aleksandr <kodochigov.aleksandr@icloud.com>
2022-06-30 21:06:53 +0300

    2nd

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Enter => ");
+string Letterdig1 = Console.ReadLine();
+int A = int.Parse(Letterdig1);
+if (A < 100)
+{
+    System.Console.WriteLine("No");
+    Environment.Exit(0);
+}
+while (A > 999) A = A / 10;
+A = A % 10;
+System.Console.WriteLine(A);
--- 4/Program.cs
+++ 4/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Enter => ");
+string Letterdig1 = Console.ReadLine();
+int A = int.Parse(Letterdig1);
+
+if(A < 1 || A > 7)
+{
+    System.Console.WriteLine("Error");
+    Environment.Exit(0);
+}
+if(A == 6 || A == 7)System.Console.WriteLine("YES");
+else System.Console.WriteLine("NO");

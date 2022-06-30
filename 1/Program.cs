bef25c226e3ad2b647fae9dbb454c1f8d0ee4a6e
Kodochigov Aleksandr <kodochigov.aleksandr@icloud.com>
2022-06-30 19:45:39 +0300

    init comm

---
+++ 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Enter => ");
+string Letterdig1 = Console.ReadLine();
+int A = int.Parse(Letterdig1);
+Console.Write(A%100/10);
